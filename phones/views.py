7d4555784b2129eb3fa8a2f3d19435c7db5411db
Danil Krutikov <kdanil.i@yandex.ru>
2023-06-07 22:42:00 +0700

    Second commit

--- phones/views.py
+++ phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,20 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones = Phone.objects.all()
+    # sort = request.GET.get['sort']
+    # if sort == 'name':
+    #     name = Phone.object.filter(name='name')
+    # elif sort == 'min_price':
+    #     min_price = Phone.object.filter()
+    # elif sort == 'max_price':
+    #     max_price = Phone.object.filter()
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug=slug)
+    context = {'phone': phone}
     return render(request, template, context)
